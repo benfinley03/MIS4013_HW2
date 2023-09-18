ee182a7136efebad4a4cf5e16491a0af2b2cdab5
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-09-18 11:52:57 -0500

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,3 +1,21 @@
+<section>
+<head>
+    <style>
+        #footer {
+            position: fixed;
+            padding: 5px 5px 0px 5px;
+            bottom: 0;
+            height: 25px;
+            background: grey;
+            right: 0;
+        }
+    </style>
+</head>
+    
+<footer id="footer">
+  <div id="footer-content"><h6>Please Read this footer: This ranking has no specific criteria and is completely based on my opinion.<h6></div>
+</footer>
+      </section>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
